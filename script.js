d051106c2c5680c7da65d1d372afb7efcbcb90ae
MrBreadcrumbs <ansel01@gmail.com>
2015-01-01 23:16:18 -0800

    Include breakdowns

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+function dollarCost (base, multiplier) {
+	var cost = base * multiplier;
+	return cost;
+}
 
 window.onload = function () {
 	var subBtn = document.getElementById('submit'); 
 	subBtn.addEventListener('click', function(event) {
 		event.preventDefault();
-		var mealCost = document.getElementById('meal').value;;
-		var taxRate = document.getElementById('tax').value;
-		var tipPercent = document.getElementById('tip').value;
-		var total = mealCost * (taxRate/100 + 1) * (tipPercent/100 + 1);
-		var amount = total.toFixed(2);
-		document.getElementById('finalCost').innerHTML = '$' + amount;
+		var mealCost = parseFloat(document.getElementById('meal').value);
+		var taxRate = parseFloat(document.getElementById('tax').value / 100);
+		var tipPercent = parseFloat(document.getElementById('tip').value / 100);
+		var taxCost = dollarCost(mealCost, taxRate);
+		var subtotal = parseFloat(mealCost) + parseFloat(taxCost);
+		var tipCost = dollarCost(subtotal, tipPercent);
+		var amount = parseFloat(subtotal + tipCost);
+
+		document.getElementById('costs').innerHTML = 
+			'<p>Meal: $' + mealCost.toFixed(2) + '</p>' +
+			'<p>Tax: $' + taxCost.toFixed(2) + '</p>' +
+			'<p>Subtotal: $' + subtotal.toFixed(2) + '</p>' +
+			'<p>Tip: $' + tipCost.toFixed(2) + '</p>' + 
+			'<p>Total: $' + amount.toFixed(2) + '</p>';
 	});
 };
